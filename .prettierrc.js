c3e61027bf21e1a355278803cdc54ed7756f5b75
Nick Dunkman <nickdunkman@gmail.com>
2025-05-31 13:55:24 -0700

    add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+
+  plugins: [
+    '@trivago/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss',
+  ],
+
+  // ** settings for @trivago/prettier-plugin-sort-imports **
+  importOrder: [
+    // third party packages at the top
+    '<THIRD_PARTY_MODULES>',
+    // TODO: Add custom import paths (components, etc)
+  ],
+  // add empty lines between the groups defined above
+  importOrderSeparation: true,
+  // sort individual pieces within each import, like:
+  // import { A, B, C } from 'some-module'
+  importOrderSortSpecifiers: true,
+
+  // ** settings for prettier-plugin-tailwindcss **
+  // automatically sort Tailwind classes passed to the className prop or the
+  // clsx() function, according to Tailwind's preferred ordering
+  tailwindAttributes: ['className'],
+  tailwindFunctions: ['clsx'],
+
+  overrides: [
+    {
+      files: ['*.js'],
+      options: {
+        trailingComma: 'es5',
+      },
+    },
+  ],
+};
