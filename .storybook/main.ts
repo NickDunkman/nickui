b655938e60ef5d844d8b67319fb5d8d61f105b27
Nick Dunkman <nickdunkman@gmail.com>
2025-06-01 16:03:29 -0700

    remove chromatic addon

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,13 +1,12 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(mdx|ts|tsx)'],
   addons: [
     '@storybook/addon-onboarding',
-    '@chromatic-com/storybook',
     '@storybook/addon-docs',
     '@storybook/addon-a11y',
-    '@storybook/addon-vitest',
+    '@storybook/addon-vitest'
   ],
   framework: {
     name: '@storybook/react-vite',
