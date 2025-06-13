dbe68b6c31d3866855880590c0e2f93199a36d27
Nick Dunkman <nickdunkman@gmail.com>
2025-06-13 12:11:02 -0700

    Add Textarea styles

---
+++ src/components/Textarea/styles.ts
@@ -0,0 +1,70 @@
+import { tv } from 'tailwind-variants';
+
+import { FieldSizer } from '@/constants';
+
+/** tailwind-variants styles for the Textarea component */
+export const styles = tv({
+  slots: {
+    root: 'relative',
+    textarea: `
+      border-black rounded-sm
+      placeholder:opacity-100 placeholder:text-gray-400
+      focus:border-blue-800 focus:shadow-[0_0_11px] focus:shadow-blue-300 focus:outline-0
+      disabled:bg-gray-100 disabled:border-gray-300 disabled:text-gray-400 disabled:placeholder:text-gray-300 disabled:cursor-not-allowed disabled:opacity-100
+    `,
+    hiddenTextarea: 'invisible absolute -z-1 pointer-events-none select-none',
+  },
+  // Some of the styles applied to textarea should also be applied to
+  // hiddenTextarea, so that the height caculations on hiddenTextarea are
+  // accurate
+  compoundSlots: [
+    {
+      slots: ['textarea', 'hiddenTextarea'],
+      className: 'appearance-none w-full border-2',
+    },
+    {
+      slots: ['textarea', 'hiddenTextarea'],
+      sizer: FieldSizer.small,
+      className: 'text-sm py-1.5 px-2',
+    },
+    {
+      slots: ['textarea', 'hiddenTextarea'],
+      sizer: FieldSizer.medium,
+      className: 'text-md py-2.5 px-3.5',
+    },
+    {
+      slots: ['textarea', 'hiddenTextarea'],
+      sizer: FieldSizer.large,
+      className: 'text-lg py-3.5 px-4.5',
+    },
+  ],
+
+  variants: {
+    sizer: {
+      [FieldSizer.small]: {},
+      [FieldSizer.medium]: {},
+      [FieldSizer.large]: {},
+    },
+    hasError: {
+      false: {},
+      true: {
+        textarea: 'border-red-800 focus:border-red-800 focus:shadow-red-300',
+      },
+    },
+    disableManualResize: {
+      false: {},
+      true: {
+        textarea: 'resize-none',
+      },
+    },
+  },
+  defaultVariants: {
+    sizer: FieldSizer.small,
+    hasError: false,
+  },
+});
+
+//.Textarea--no-manual-resize {
+//  resize: none;
+//}
+//
