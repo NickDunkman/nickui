465d5db1ad094545415458e6aade1bd5af25c945
Nick Dunkman <nickdunkman@gmail.com>
2025-06-30 18:34:25 -0700

    compile docs

---
+++ docs/assets/Button-yX0g6uH_.js
@@ -0,0 +1,12 @@
+import{j as o,M as c,C as f}from"./blocks-Du9aBEOj.js";import{useMDXComponents as a}from"./index-YzsNL2Pu.js";import{D as e}from"./index-CcB7iNqj.js";import{B as d}from"./Button-whMqm6bc.js";import{S as r,A as l}from"./Button.stories-DCdoirIS.js";import"./iframe-Cu56G6_x.js";import"./index-B3FxOEC8.js";import"./client-Dzx5Y50O.js";import"./index-kiPNViGW.js";import"./index-BiqwU_Yq.js";import"./AllSizersStoryWrapper-o0UCkeT9.js";function i(t){const n={code:"code",h2:"h2",p:"p",...a(),...t.components};return e||s("Docs",!1),e.ComponentHeading||s("Docs.ComponentHeading",!0),e.ControlStates||s("Docs.ControlStates",!0),e.FauxCanvas||s("Docs.FauxCanvas",!0),e.Props||s("Docs.Props",!0),e.Sizers||s("Docs.Sizers",!0),e.TableOfContents||s("Docs.TableOfContents",!0),o.jsxs(o.Fragment,{children:[o.jsx(c,{of:r}),`
+`,o.jsx(e.ComponentHeading,{of:"Button"}),`
+`,o.jsx(n.p,{children:"Allows the user to trigger an action, such as submitting a form."}),`
+`,o.jsx(e.FauxCanvas,{children:o.jsx(d,{children:"Click me?"})}),`
+`,o.jsx(e.TableOfContents,{}),`
+`,o.jsx(e.Props,{of:r,nativeBase:"button"}),`
+`,o.jsx(e.ControlStates,{of:r}),`
+`,o.jsx(n.h2,{id:"flavors",children:"Flavors"}),`
+`,o.jsxs(n.p,{children:["Coloring is done via the ",o.jsx(n.code,{children:"flavor"})," prop. You can pass ",o.jsx(n.code,{children:'"primary"'}),` (the default),
+`,o.jsx(n.code,{children:'"secondary"'}),", or ",o.jsx(n.code,{children:'"danger"'}),"."]}),`
+`,o.jsx(f,{of:l}),`
+`,o.jsx(e.Sizers,{of:r})]})}function b(t={}){const{wrapper:n}={...a(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(i,{...t})}):i(t)}function s(t,n){throw new Error("Expected "+(n?"component":"object")+" `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{b as default};
---
+++ docs/assets/Switch-CErdjuZL.js
@@ -0,0 +1,12 @@
+import{j as o,M as f}from"./blocks-Du9aBEOj.js";import{useMDXComponents as a}from"./index-YzsNL2Pu.js";import{D as e}from"./index-CcB7iNqj.js";import{S as c}from"./Switch-BpySIMEf.js";import{S as i}from"./Switch.stories-GDVMu6CP.js";import"./iframe-Cu56G6_x.js";import"./index-B3FxOEC8.js";import"./client-Dzx5Y50O.js";import"./index-kiPNViGW.js";import"./Button-whMqm6bc.js";import"./index-BiqwU_Yq.js";import"./Checkbox-Dpf-3Pqn.js";import"./CheckedField-Dgat-woQ.js";import"./randomId-Dfhrkieu.js";import"./AllSizersStoryWrapper-o0UCkeT9.js";function s(t){const n={p:"p",...a(),...t.components};return e||r("Docs",!1),e.ComponentHeading||r("Docs.ComponentHeading",!0),e.ControlStates||r("Docs.ControlStates",!0),e.FauxCanvas||r("Docs.FauxCanvas",!0),e.FormLibraryCompatibility||r("Docs.FormLibraryCompatibility",!0),e.Props||r("Docs.Props",!0),e.Sizers||r("Docs.Sizers",!0),e.TableOfContents||r("Docs.TableOfContents",!0),o.jsxs(o.Fragment,{children:[o.jsx(f,{of:i}),`
+`,o.jsx(e.ComponentHeading,{of:"Switch"}),`
+`,o.jsxs(n.p,{children:["A variation of ",o.jsx(e,{of:"Checkbox"}),` that has a more explict-feeling "off"
+state. Whereas a group of co-located `,o.jsx(e,{of:"Checkbox"}),` feels like different
+options under one field, a group of `,o.jsx(e,{of:"Switch"}),` feels more like
+discrete fields under a common category.`]}),`
+`,o.jsx(e.FauxCanvas,{children:o.jsx(c,{label:"Switch me"})}),`
+`,o.jsx(e.TableOfContents,{}),`
+`,o.jsx(e.Props,{of:i,wrapper:"CheckedField",nativeBase:"input"}),`
+`,o.jsx(e.ControlStates,{of:i,checkable:!0}),`
+`,o.jsx(e.Sizers,{of:i,wrapper:"CheckedField"}),`
+`,o.jsx(e.FormLibraryCompatibility,{of:i,fieldName:"liftCleanAndPlace",checkbox:!0,initialValue:!0,componentProps:{label:"Lift, clean, and place in effect"}})]})}function w(t={}){const{wrapper:n}={...a(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(s,{...t})}):s(t)}function r(t,n){throw new Error("Expected "+(n?"component":"object")+" `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{w as default};
