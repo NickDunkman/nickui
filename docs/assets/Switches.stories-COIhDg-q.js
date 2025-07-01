d65c79ff540313803b4c4da16c70ff893cee1ac0
Nick Dunkman <nickdunkman@gmail.com>
2025-06-30 18:34:25 -0700

    compile docs

---
+++ docs/assets/Switches.stories-COIhDg-q.js
@@ -0,0 +1,120 @@
+import{r as e}from"./iframe-BOtqaFuv.js";import{S as d}from"./Switch-BFmcoUnH.js";import{F as A}from"./index-Czi7Upa3.js";import{a as P}from"./Checkboxes-CyVie92U.js";function a({delimiter:t=",",...L}){return e.createElement(P,{...L,Checkable:d,delimiter:t})}a.sizer=A;a.__docgenInfo={description:`Renders multiple <Switch> as one field in a <Fieldset>.
+@props {@link CheckablesProps} + {@link CommonFieldsetProps}`,methods:[],displayName:"Switches",props:{name:{required:!1,tsType:{name:"string"},description:"The name for the field"},render:{required:!1,tsType:{name:"signature",type:"function",raw:`(
+  inputProps: (control: {
+    value: string;
+    disabled?: boolean;
+  }) => React.ComponentProps<'input'>,
+) => React.ReactNode`,signature:{arguments:[{type:{name:"signature",type:"function",raw:`(control: {
+  value: string;
+  disabled?: boolean;
+}) => React.ComponentProps<'input'>`,signature:{arguments:[{type:{name:"signature",type:"object",raw:`{
+  value: string;
+  disabled?: boolean;
+}`,signature:{properties:[{key:"value",value:{name:"string",required:!0}},{key:"disabled",value:{name:"boolean",required:!1}}]}},name:"control"}],return:{name:"ReactComponentProps",raw:"React.ComponentProps<'input'>",elements:[{name:"literal",value:"'input'"}]}}},name:"inputProps"}],return:{name:"ReactReactNode",raw:"React.ReactNode"}}},description:`Render function used to render customized checkable inputs. Call the
+provided callback to get props to pass down to each input.`},options:{required:!1,tsType:{name:"Array",elements:[{name:"signature",type:"object",raw:`{
+  value: string;
+  label: React.ReactNode;
+  hint?: React.ReactNode;
+  disabled?: boolean;
+}`,signature:{properties:[{key:"value",value:{name:"string",required:!0}},{key:"label",value:{name:"ReactReactNode",raw:"React.ReactNode",required:!0}},{key:"hint",value:{name:"ReactReactNode",raw:"React.ReactNode",required:!1}},{key:"disabled",value:{name:"boolean",required:!1}}]}}],raw:`{
+  value: string;
+  label: React.ReactNode;
+  hint?: React.ReactNode;
+  disabled?: boolean;
+}[]`},description:"Adds the controls in the standard layout"},onChange:{required:!1,tsType:{name:"ReactChangeEventHandler",raw:"React.ChangeEventHandler<HTMLInputElement>",elements:[{name:"HTMLInputElement"}]},description:"Called when the value changes"},value:{required:!1,tsType:{name:"string"},description:`Sets the value when using it as a
+[controlled component](https://react.dev/reference/react-dom/components/input#controlling-an-input-with-a-state-variable)`},defaultValue:{required:!1,tsType:{name:"string"},description:"Sets the value when using it as an uncontrolled component"},delimiter:{required:!1,tsType:{name:"string"},description:`Optionally override the delimiter used to split the value into individual
+checkable values`,defaultValue:{value:"','",computed:!1}},className:{required:!1,tsType:{name:"string"},description:"Optionally add utility classes to the root element"},sizer:{required:!1,tsType:{name:"FieldSizer"},description:'Changes the size of the field ("small", "medium", "large")'},label:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:"The main label to show above the form control, naming the field"},hint:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:`A hint about what kind of value the control accepts, such as a phone
+number format`},error:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:"Error message to show along with the field"},required:{required:!1,tsType:{name:"boolean"},description:"Adds an asterisk to the label"},disabled:{required:!1,tsType:{name:"boolean"},description:"Prevents the user from interacting with the control"}},composes:["Omit"]};const M={title:"Form controls/Switches",component:a},r={args:{label:"Please select some standards",defaultValue:"8601",options:[{value:"aquila",label:"Roman Aquila"},{value:"8601",label:"ISO 8601"},{value:"high-society",label:"High Society",disabled:!0}]}},o={args:{label:"Please select some customizations",defaultValue:"colorful,embiggened",render:t=>e.createElement("div",{className:"flex flex-row items-baseline gap-x-6"},e.createElement(d,{...t({value:"colorful"}),label:e.createElement("span",{className:"font-bold text-blue-500"},"Colorful"),className:"rounded-sm bg-rose-200 p-2"}),e.createElement(d,{...t({value:"embiggened"}),sizer:d.sizer.large,label:"Embiggened"}),e.createElement("div",null,e.createElement("input",{type:"checkbox",...t({value:"native"})})," Native"))}},i={args:{label:"A label",hint:"A hint",error:"An error message",options:[{value:"one",label:"Option one"},{value:"two",label:"Option two"}]}},n={args:{label:"Small label",hint:"Small hint",sizer:a.sizer.small,options:[{value:"one",label:"Option one"},{value:"two",label:"Option two"}]}},l={args:{label:"Medium label",hint:"Medium hint",sizer:a.sizer.medium,options:[{value:"one",label:"Option one"},{value:"two",label:"Option two"}]}},s={args:{label:"Large label",hint:"Large hint",sizer:a.sizer.large,options:[{value:"one",label:"Option one"},{value:"two",label:"Option two"}]}},c={tags:["!dev","!test"],render:t=>e.createElement("div",{className:"flex flex-col gap-x-12 gap-y-8 sm:flex-row sm:items-baseline"},e.createElement(a,{...n.args,className:"sm:flex-1"}),e.createElement(a,{...l.args,className:"sm:flex-1"}),e.createElement(a,{...s.args,className:"sm:flex-1"}))};var u,m,p;r.parameters={...r.parameters,docs:{...(u=r.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  args: {
+    label: 'Please select some standards',
+    defaultValue: '8601',
+    options: [{
+      value: 'aquila',
+      label: 'Roman Aquila'
+    }, {
+      value: '8601',
+      label: 'ISO 8601'
+    }, {
+      value: 'high-society',
+      label: 'High Society',
+      disabled: true
+    }]
+  }
+}`,...(p=(m=r.parameters)==null?void 0:m.docs)==null?void 0:p.source}}};var b,g,h;o.parameters={...o.parameters,docs:{...(b=o.parameters)==null?void 0:b.docs,source:{originalSource:`{
+  args: {
+    label: 'Please select some customizations',
+    defaultValue: 'colorful,embiggened',
+    render: checkbox => <div className="flex flex-row items-baseline gap-x-6">
+        <Switch {...checkbox({
+        value: 'colorful'
+      })} label={<span className="font-bold text-blue-500">Colorful</span>} className="rounded-sm bg-rose-200 p-2" />
+        <Switch {...checkbox({
+        value: 'embiggened'
+      })} sizer={Switch.sizer.large} label="Embiggened" />
+        <div>
+          <input type="checkbox" {...checkbox({
+          value: 'native'
+        })} /> Native
+        </div>
+      </div>
+  }
+}`,...(h=(g=o.parameters)==null?void 0:g.docs)==null?void 0:h.source}}};var v,f,w;i.parameters={...i.parameters,docs:{...(v=i.parameters)==null?void 0:v.docs,source:{originalSource:`{
+  args: {
+    label: 'A label',
+    hint: 'A hint',
+    error: 'An error message',
+    options: [{
+      value: 'one',
+      label: 'Option one'
+    }, {
+      value: 'two',
+      label: 'Option two'
+    }]
+  }
+}`,...(w=(f=i.parameters)==null?void 0:f.docs)==null?void 0:w.source}}};var y,S,R;n.parameters={...n.parameters,docs:{...(y=n.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    label: 'Small label',
+    hint: 'Small hint',
+    sizer: Switches.sizer.small,
+    options: [{
+      value: 'one',
+      label: 'Option one'
+    }, {
+      value: 'two',
+      label: 'Option two'
+    }]
+  }
+}`,...(R=(S=n.parameters)==null?void 0:S.docs)==null?void 0:R.source}}};var N,x,z;l.parameters={...l.parameters,docs:{...(N=l.parameters)==null?void 0:N.docs,source:{originalSource:`{
+  args: {
+    label: 'Medium label',
+    hint: 'Medium hint',
+    sizer: Switches.sizer.medium,
+    options: [{
+      value: 'one',
+      label: 'Option one'
+    }, {
+      value: 'two',
+      label: 'Option two'
+    }]
+  }
+}`,...(z=(x=l.parameters)==null?void 0:x.docs)==null?void 0:z.source}}};var q,O,k;s.parameters={...s.parameters,docs:{...(q=s.parameters)==null?void 0:q.docs,source:{originalSource:`{
+  args: {
+    label: 'Large label',
+    hint: 'Large hint',
+    sizer: Switches.sizer.large,
+    options: [{
+      value: 'one',
+      label: 'Option one'
+    }, {
+      value: 'two',
+      label: 'Option two'
+    }]
+  }
+}`,...(k=(O=s.parameters)==null?void 0:O.docs)==null?void 0:k.source}}};var E,C,T;c.parameters={...c.parameters,docs:{...(E=c.parameters)==null?void 0:E.docs,source:{originalSource:`{
+  tags: ['!dev', '!test'],
+  render: _args => <div className="flex flex-col gap-x-12 gap-y-8 sm:flex-row sm:items-baseline">
+      <Switches {...Small.args} className="sm:flex-1" />
+      <Switches {...Medium.args} className="sm:flex-1" />
+      <Switches {...Large.args} className="sm:flex-1" />
+    </div>
+}`,...(T=(C=c.parameters)==null?void 0:C.docs)==null?void 0:T.source}}};const _=["StandardLayout","CustomLayout","FieldsetLayout","Small","Medium","Large","AllSizes"],j=Object.freeze(Object.defineProperty({__proto__:null,AllSizes:c,CustomLayout:o,FieldsetLayout:i,Large:s,Medium:l,Small:n,StandardLayout:r,__namedExportsOrder:_,default:M},Symbol.toStringTag,{value:"Module"}));export{o as C,j as S,a,r as b};
