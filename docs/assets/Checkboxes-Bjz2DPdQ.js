bdf21d37a5d356dbcaff15a99111650d0af9c5f7
Nick Dunkman <nickdunkman@gmail.com>
2025-06-30 18:34:25 -0700

    compile docs

---
+++ docs/assets/Checkboxes-Bjz2DPdQ.js
@@ -0,0 +1,67 @@
+import{j as e,M as h,C as r,S as i}from"./blocks-Dl-vJRMD.js";import{useMDXComponents as u}from"./index-zuAqXSsy.js";import{D as o}from"./index-C4SKkLfP.js";import{s as a,C as l,a as m,c as b}from"./Checkboxes.stories-DQvoiuno.js";import"./preload-helper-D9Z9MdNV.js";import"./iframe-D_OX4RS-.js";import"./index-BZe1LVJg.js";import"./client-CXUqmXYr.js";import"./Button-DtBrz5VC.js";import"./index-D7mztUp4.js";import"./index-AP4d4dr6.js";import"./Checkbox-WodopptC.js";import"./CheckableFormControl-DwD9qaWp.js";import"./fieldA11y-CxK2--J7.js";import"./randomId-Dfhrkieu.js";import"./CheckedField-DKfyikG7.js";import"./useElementBounds-BwtEEY9F.js";import"./AllSizersStoryWrapper-Djlh-4yQ.js";import"./getStoryArgTypes-CB50-RjO.js";import"./CheckableFormControls-D4Va4pKI.js";import"./Fieldset-BEqBYkle.js";function c(s){const t={code:"code",h2:"h2",p:"p",...u(),...s.components};return o||n("Docs",!1),o.ComponentHeading||n("Docs.ComponentHeading",!0),o.ControlStates||n("Docs.ControlStates",!0),o.FauxCanvas||n("Docs.FauxCanvas",!0),o.FormLibraryDemos||n("Docs.FormLibraryDemos",!0),o.Props||n("Docs.Props",!0),o.Sizers||n("Docs.Sizers",!0),o.TableOfContents||n("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(h,{of:a}),`
+`,e.jsx(o.ComponentHeading,{of:"Checkboxes"}),`
+`,e.jsx("div",{children:e.jsxs(t.p,{children:["Renders multiple ",e.jsx(o,{of:"Checkbox"})," that perform as one field in a"," ",`
+`,e.jsx(o,{of:"Fieldset"}),"."]})}),`
+`,e.jsx(o.FauxCanvas,{children:e.jsx(l,{label:"Add some toppings to your Burgerdog",defaultValue:"sauerkraut,mustard",options:[{value:"sauerkraut",label:"Sauerkraut"},{value:"relish",label:"Relish"},{value:"onions",label:"Onions"},{value:"mustard",label:"Mustard"},{value:"grey-poupon",label:"Grey Poupon 🥸"}]})}),`
+`,e.jsx(o.TableOfContents,{}),`
+`,e.jsx(o.Props,{of:a,nativeBase:"input",wrapper:"Fieldset"}),`
+`,e.jsx(o.ControlStates,{of:a,valuable:!0,sourceState:"hidden",note:"Note: while you can disable individual checkboxes as shown in sections below, using `disabled={true}` will disable the entire field by forcing all child checkboxes to be disabled."}),`
+`,e.jsx(t.h2,{id:"adding-checkboxes",children:"Adding checkboxes"}),`
+`,e.jsxs(t.p,{children:["For the standard look & feel, use the ",e.jsx(t.code,{children:"options"}),` prop to configure the individual
+checkboxes. `,e.jsx(o,{of:"Checkboxes"})," will manage the interior layout."]}),`
+`,e.jsx(r,{of:m,sourceState:"shown"}),`
+`,e.jsxs(t.p,{children:[`To customize the checkbox controls, such as to use a different component for the
+individual checkboxes, call the props differently, or add other elements, you
+can pass a `,e.jsx(t.code,{children:"render"})," function instead of using ",e.jsx(t.code,{children:"options"}),"."]}),`
+`,e.jsx("strong",{children:e.jsx(t.p,{children:`As shown, it’s important to use the props callback provided for the field to
+behave properly.`})}),`
+`,e.jsx(r,{of:b,sourceState:"none"}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+  <Checkboxes
+    label="How custom do you like it?"
+    defaultValue="colorful,embiggened"
+    render={(checkbox) => (
+      <div className="flex flex-row items-baseline gap-x-6">
+        <Checkbox
+          {...checkbox({ value: 'colorful' })}
+          label={<span className="text-blue-700 font-bold">Colorful</span>}
+          className="rounded-sm bg-rose-200 p-2"
+        />
+        <Checkbox
+          {...checkbox({ value: 'embiggened' })}
+          sizer="lg"
+          label="Embiggened"
+        />
+        <label>
+          <input type="checkbox" {...checkbox({ value: 'native' })} /> Native
+        </label>
+      </div>
+    )}
+  />
+`}),`
+`,e.jsx(t.h2,{id:"working-with-the-value",children:"Working with the value"}),`
+`,e.jsx(t.p,{children:`The purpose of this component is to make it as easy as possible to have a
+consistent look & feel for fields with multiple checkboxes. In order to simplify
+the boilerplate needed for libraries like Formik and React Hook Form, the
+component’s value is a single delimited-string, & toggling each individual
+checkbox adds or removes a particular substring.`}),`
+`,e.jsx(t.p,{children:`If you want to work with an array of strings instead of a combined string, you
+can convert to/from an array somewhere higher up the tree, as shown in this
+example:`}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+  const [value, setValue] = React.useState<string[]>(['a', 'c']);
+  //...
+  <Checkboxes
+    options={[
+      { value: 'a', label: 'A' },
+      { value: 'b', label: 'B' },
+      { value: 'c', label: 'D' },
+    ]}
+    value={value.join(',')}
+    onChange={(event) => {
+      setValue(event.target.value.split(','));
+    }}
+  />
+`}),`
+`,e.jsx(o.Sizers,{of:a,wrapper:"Fieldset"}),`
+`,e.jsx(o.FormLibraryDemos,{of:"Checkboxes",fieldName:"courseFeatures",initialValue:"pot-bunkers,undulated-greens",children:({props:d,error:p})=>e.jsx(l,{...d,label:"Course features",hint:"Try editting me!",error:p,options:[{value:"pot-bunkers",label:"Pot bunkers"},{value:"water",label:"Water hazards"},{value:"undulated-greens",label:"Undulated greens"}]})})]})}function A(s={}){const{wrapper:t}={...u(),...s.components};return t?e.jsx(t,{...s,children:e.jsx(c,{...s})}):c(s)}function n(s,t){throw new Error("Expected "+(t?"component":"object")+" `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{A as default};
---
+++ docs/assets/Fieldset-Cv9V9n8V.js
@@ -0,0 +1,17 @@
+import{j as e,M as d,C as a}from"./blocks-Dl-vJRMD.js";import{useMDXComponents as l}from"./index-zuAqXSsy.js";import{D as s}from"./index-C4SKkLfP.js";import{F as t}from"./FormControlPlaceholder-DrUFKhTN.js";import"./Radios-hJ2QaOi5.js";import{F as m}from"./Fieldset-BEqBYkle.js";import{s as i,b as p,r as f}from"./Fieldset.stories-wT0Ii816.js";import"./preload-helper-D9Z9MdNV.js";import"./iframe-D_OX4RS-.js";import"./index-BZe1LVJg.js";import"./client-CXUqmXYr.js";import"./Button-DtBrz5VC.js";import"./index-D7mztUp4.js";import"./index-AP4d4dr6.js";import"./CheckableFormControls-D4Va4pKI.js";import"./randomId-Dfhrkieu.js";import"./Radio-VqvZoY1C.js";import"./CheckableFormControl-DwD9qaWp.js";import"./fieldA11y-CxK2--J7.js";import"./CheckedField-DKfyikG7.js";import"./useElementBounds-BwtEEY9F.js";import"./AllSizersStoryWrapper-Djlh-4yQ.js";function c(r){const o={code:"code",h2:"h2",p:"p",...l(),...r.components};return s||n("Docs",!1),s.ComponentHeading||n("Docs.ComponentHeading",!0),s.FauxCanvas||n("Docs.FauxCanvas",!0),s.Props||n("Docs.Props",!0),s.Sizers||n("Docs.Sizers",!0),s.TableOfContents||n("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(d,{of:i}),`
+`,e.jsx(s.ComponentHeading,{of:"Fieldset"}),`
+`,e.jsx(o.p,{children:`Wraps multiple form controls in a standard field layout (label, hint, error
+message, etc).`}),`
+`,e.jsx("div",{children:e.jsxs(o.p,{children:["Note: this layout is already built-into ",e.jsx(s,{of:"Checkboxes"}),`,
+`,e.jsx(s,{of:"Radios"}),", and ",e.jsx(s,{of:"Switches"}),`. You can use this component
+to create custom form controls that behave the same way.`]})}),`
+`,e.jsx(s.FauxCanvas,{children:e.jsx(m,{label:"A label",hint:"A hint",error:"An error message!",children:e.jsxs("div",{className:"flex flex-col gap-3",children:[e.jsx(t,{children:"One form control"}),e.jsx(t,{children:"Another form control"}),e.jsx(t,{children:"And so on ..."})]})})}),`
+`,e.jsx(s.TableOfContents,{}),`
+`,e.jsx(s.Props,{of:i,nativeBase:"fieldset"}),`
+`,e.jsx(o.h2,{id:"basic-elements",children:"Basic elements"}),`
+`,e.jsxs(o.p,{children:["These elements are controlled using the ",e.jsx(o.code,{children:"label"}),", ",e.jsx(o.code,{children:"hint"}),", and ",e.jsx(o.code,{children:"error"})," props."]}),`
+`,e.jsx(a,{of:p,sourceState:"shown"}),`
+`,e.jsx(s.Sizers,{of:i}),`
+`,e.jsx(o.h2,{id:"required-fields",children:"Required fields"}),`
+`,e.jsxs(o.p,{children:["The ",e.jsx(o.code,{children:"required"})," prop is used to add an asterisk to the label."]}),`
+`,e.jsx(a,{of:f})]})}function E(r={}){const{wrapper:o}={...l(),...r.components};return o?e.jsx(o,{...r,children:e.jsx(c,{...r})}):c(r)}function n(r,o){throw new Error("Expected "+(o?"component":"object")+" `"+r+"` to be defined: you likely forgot to import, pass, or provide it.")}export{E as default};
