46f3a406e14a8f8ed88aa39d344248d252653890
Nick Dunkman <nickdunkman@gmail.com>
2025-07-04 17:30:11 -0700

    get ComponentName working in static storybook build (dynamic imports don't work)

--- src/docs/ComponentName.tsx
+++ src/docs/ComponentName.tsx
@@ -1,29 +1,11 @@
 import { AnchorMdx } from '@storybook/addon-docs/blocks';
 import * as React from 'react';
 
-function getDocsId(docsTitle: string) {
-  return `${docsTitle.replace(/\W+/g, '-').toLowerCase()}--docs`;
-}
+import { getStoryId } from '@/utils/stories';
 
 /** Given a component name, renders a fancy-looking link to its docs */
 export function ComponentName({ of }: { of: string }) {
-  const [docsTitle, setDocsTitle] = React.useState<string>();
-
-  React.useEffect(() => {
-    const componentStoriesPath = `../components/${of}/${of}.stories`;
-    import(
-      /* @vite-ignore */
-      componentStoriesPath
-    ).then((stories) => {
-      setDocsTitle(stories.default.title);
-    });
-  }, [of]);
-
-  if (!docsTitle) {
-    return null;
-  }
-
-  const docsId = getDocsId(docsTitle);
+  const docsId = getStoryId(of, 'Docs');
   const asLink = !window.location.href.includes(docsId);
 
   const componentName = (
---
+++ src/utils/stories.ts
@@ -0,0 +1,30 @@
+function sluggify(str: string) {
+  return str.replace(/\W+/g, '-').toLowerCase();
+}
+
+export function getStoryId(componentName: string, storyName: string) {
+  return `${sluggify(getStoryTitle(componentName))}--${sluggify(storyName)}`;
+}
+
+export function getStoryTitle(componentName: string) {
+  switch (componentName) {
+    case 'Button':
+    case 'Checkbox':
+    case 'Checkboxes':
+    case 'Radio':
+    case 'Radios':
+    case 'Select':
+    case 'Slider':
+    case 'Switch':
+    case 'Switches':
+    case 'Text':
+    case 'Textarea':
+      return `Form controls/${componentName}`;
+    case 'CheckedField':
+    case 'Field':
+    case 'Fieldset':
+      return `Field layouts/${componentName}`;
+    default:
+      return componentName;
+  }
+}
