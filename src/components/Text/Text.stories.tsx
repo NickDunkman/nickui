8f5ef07c0d62b43e02ccae0747ca05f0c7f9528b
Nick Dunkman <nickdunkman@gmail.com>
2025-07-08 11:16:13 -0700

    recommend vitest extension in vscode

--- src/components/Text/Text.stories.tsx
+++ src/components/Text/Text.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import * as React from 'react';
-import { expect, fn } from 'storybook/test';
+import { expect } from 'storybook/test';
 
 import { Text } from './Text';
 
