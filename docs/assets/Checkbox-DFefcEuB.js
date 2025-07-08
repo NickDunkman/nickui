7ee6efcb819942bb2997e88358ef248b89c9cf62
Nick Dunkman <nickdunkman@gmail.com>
2025-07-08 12:16:49 -0700

    update docs

---
+++ docs/assets/Checkbox-DFefcEuB.js
@@ -0,0 +1,9 @@
+import{j as t,M as p}from"./blocks-CIyDAHRV.js";import{useMDXComponents as m}from"./index-BtNVSfKj.js";import{D as o}from"./index-PYPD6cJ5.js";import{C as s}from"./Checkbox-Cbwsbf_B.js";import{S as i}from"./Checkbox.stories-45M0vrpb.js";import"./iframe-HsRp2Cui.js";import"./index-_LN8wga_.js";import"./clsw-D-KgF5mV.js";import"./Button-D5GEWY_9.js";import"./index-Czi7Upa3.js";import"./CheckedField-Bi2rQZlu.js";function a(e){const r={p:"p",...m(),...e.components};return o||n("Docs",!1),o.ComponentHeading||n("Docs.ComponentHeading",!0),o.ControlStates||n("Docs.ControlStates",!0),o.FauxCanvas||n("Docs.FauxCanvas",!0),o.FormLibraryCompatibility||n("Docs.FormLibraryCompatibility",!0),o.Props||n("Docs.Props",!0),o.Sizing||n("Docs.Sizing",!0),o.TableOfContents||n("Docs.TableOfContents",!0),t.jsxs(t.Fragment,{children:[t.jsx(p,{of:i}),`
+`,t.jsx(o.ComponentHeading,{of:"Checkbox"}),`
+`,t.jsx(r.p,{children:"A form control that allow users to choose zero to many options from a set."}),`
+`,t.jsx(o.FauxCanvas,{children:t.jsx(s,{label:"Don’t not opt out of my newsletter 😈",sizer:s.sizer.medium})}),`
+`,t.jsx(o.TableOfContents,{}),`
+`,t.jsx(o.Props,{of:i,wrapper:"CheckedField",nativeBase:"input"}),`
+`,t.jsx(o.ControlStates,{of:i}),`
+`,t.jsx(o.Sizing,{of:i,wrapper:"CheckedField"}),`
+`,t.jsx(o.FormLibraryCompatibility,{of:i,fieldName:"gimme",checkbox:!0,initialValue:!0,componentProps:{label:"That’s a gimme"}})]})}function g(e={}){const{wrapper:r}={...m(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(a,{...e})}):a(e)}function n(e,r){throw new Error("Expected "+(r?"component":"object")+" `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{g as default};
