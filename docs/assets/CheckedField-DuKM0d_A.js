58f111dd51780d91198dc95d598808bd3971f1e7
Nick Dunkman <nickdunkman@gmail.com>
2025-07-10 13:06:18 -0700

    update docs

---
+++ docs/assets/CheckedField-DuKM0d_A.js
@@ -0,0 +1,18 @@
+import{j as e,M as l,C as d}from"./blocks-C9VVuW9P.js";import{useMDXComponents as c}from"./index-Cxw9u9EW.js";import{D as o}from"./index-CHuDmDyx.js";import{C as r}from"./CheckedField-Bv-NBShP.js";import{S as i,B as h}from"./CheckedField.stories-gT4Yg5gg.js";import"./iframe-B-Uinc_h.js";import"./index-D6UOlYAK.js";import"./clsw-D-KgF5mV.js";import"./Button-BdzfBpjT.js";import"./index-Czi7Upa3.js";function a(t){const n={code:"code",h2:"h2",p:"p",...c(),...t.components};return o||s("Docs",!1),o.ComponentHeading||s("Docs.ComponentHeading",!0),o.FauxCanvas||s("Docs.FauxCanvas",!0),o.FormControlPlaceholder||s("Docs.FormControlPlaceholder",!0),o.Props||s("Docs.Props",!0),o.Sizing||s("Docs.Sizing",!0),o.TableOfContents||s("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(l,{of:i}),`
+`,e.jsx(o.ComponentHeading,{of:"CheckedField"}),`
+`,e.jsx(n.p,{children:`Wraps a single checkable form control with a standard layout (label & hint, to
+the right of the control).`}),`
+`,e.jsxs(n.p,{children:["This is simpler than the ",e.jsx(o,{of:"Field"})," component. There’s no ",e.jsx(n.code,{children:"error"}),`
+message, since if one or both possible values is invalid, the field has no
+purpose. And there’s no `,e.jsx(n.code,{children:"required"}),` state, since that would make the unchecked
+state invalid.`]}),`
+`,e.jsx("div",{children:e.jsxs(n.p,{children:["Note: this layout is already built-into ",e.jsx(o,{of:"Checkbox"}),","," ",`
+`,e.jsx(o,{of:"Radio"}),", and ",e.jsx(o,{of:"Switch"}),`. You can use this component to
+create a custom form control that behaves the same way.`]})}),`
+`,e.jsx(o.FauxCanvas,{children:e.jsx(r,{label:"A label",hint:"A hint",sizer:r.sizer.medium,children:e.jsx(o.FormControlPlaceholder,{tiny:!0})})}),`
+`,e.jsx(o.TableOfContents,{}),`
+`,e.jsx(o.Props,{of:i,nativeBase:"label"}),`
+`,e.jsx(n.h2,{id:"basic-elements",children:"Basic elements"}),`
+`,e.jsxs(n.p,{children:["These elements are controlled using the ",e.jsx(n.code,{children:"label"})," and ",e.jsx(n.code,{children:"hint"})," props."]}),`
+`,e.jsx(d,{of:h,sourceState:"shown"}),`
+`,e.jsx(o.Sizing,{of:i})]})}function v(t={}){const{wrapper:n}={...c(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(a,{...t})}):a(t)}function s(t,n){throw new Error("Expected "+(n?"component":"object")+" `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{v as default};
