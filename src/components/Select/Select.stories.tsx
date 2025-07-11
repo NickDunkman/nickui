7663646a81f31aef34b5aa45a8e8cdace73c06f3
Nick Dunkman <nickdunkman@gmail.com>
2025-07-11 13:14:33 -0700

    Add story tests for Select

--- src/components/Select/Select.stories.tsx
+++ src/components/Select/Select.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react-vite';
 import * as React from 'react';
+import { expect, fn } from 'storybook/test';
 
 import { Select } from './Select';
 
@@ -12,7 +13,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const FieldLayout: Story = {
-  tags: ['!dev', '!test'],
   args: {
     required: true,
     label: 'A label',
@@ -20,13 +20,28 @@ export const FieldLayout: Story = {
     hint: 'A hint',
     children: <option>An option</option>,
   },
+  play: async ({ canvas, step }) => {
+    const select = canvas.getByLabelText('A label*');
+    const requiredAsterisk = canvas.getByTitle('required');
+
+    await step('Assert accessibility of layout elements', async () => {
+      expect(select).toHaveRole('combobox');
+      expect(select).toHaveAccessibleDescription('A hint');
+      expect(select).toHaveAccessibleErrorMessage('An error message');
+      expect(requiredAsterisk).toHaveTextContent('*');
+    });
+
+    await step('Assert the error style', async () => {
+      expect(select).toHaveClass('border-rose-800');
+    });
+  },
 };
 
 export const Controlled: Story = {
   args: {
     value: 'controlled',
-    onChange: () => {},
-    'aria-label': 'Example Select with a controlled value',
+    onChange: fn(),
+    'aria-label': 'Controlled Select',
     children: [
       <option value="another" key="another">
         Another option
@@ -36,12 +51,32 @@ export const Controlled: Story = {
       </option>,
     ],
   },
+  play: async ({ args, canvas, step, userEvent }) => {
+    const select = canvas.getByLabelText('Controlled Select');
+
+    await step('Assert `value` works', async () => {
+      expect(select).toHaveValue(args.value);
+    });
+
+    await step(
+      'Try selecting the other option. `onChange` should fire, but the value is controlled, so it shouldn’t change',
+      async () => {
+        await userEvent.tab();
+        expect(select).toHaveFocus();
+
+        await userEvent.selectOptions(select, 'another');
+        expect(args.onChange).toHaveBeenCalledOnce();
+        expect(select).toHaveValue(args.value);
+      },
+    );
+  },
 };
 
 export const Uncontrolled: Story = {
   args: {
     defaultValue: 'uncontrolled',
-    'aria-label': 'Example Select with an uncontrolled value',
+    'aria-label': 'Uncontrolled Select',
+    onChange: fn(),
     children: [
       <option value="another" key="another">
         Another option
@@ -51,13 +86,91 @@ export const Uncontrolled: Story = {
       </option>,
     ],
   },
+  play: async ({ args, canvas, step, userEvent }) => {
+    const select = canvas.getByLabelText('Uncontrolled Select');
+
+    await step('Assert `defaultValue` works', async () => {
+      expect(select).toHaveValue(args.defaultValue);
+    });
+
+    await step(
+      'Selecting a different option should change the value, since it’s uncontrolled',
+      async () => {
+        await userEvent.selectOptions(select, 'another');
+        expect(select).toHaveValue('another');
+        expect(args.onChange).toHaveBeenCalledOnce();
+      },
+    );
+
+    await step('Reset the value', async () => {
+      await userEvent.selectOptions(select, 'uncontrolled');
+      expect(select).toHaveValue('uncontrolled');
+    });
+  },
 };
 
 export const Disabled: Story = {
   args: {
     disabled: true,
-    'aria-label': 'Example disabled Select',
-    children: <option>Disabled Select</option>,
+    'aria-label': 'Disabled Select',
+    onChange: fn(),
+    children: <option value="disabled">Disabled Select</option>,
+  },
+  play: async ({ canvas, step }) => {
+    await step('Assert disabled state', async () => {
+      expect(canvas.getByLabelText('Disabled Select')).toBeDisabled();
+    });
+  },
+};
+
+export const Small: Story = {
+  args: {
+    label: 'Small label',
+    sizer: Select.sizer.small,
+    children: <option>Small Select (default)</option>,
+  },
+  play: async ({ canvas, step }) => {
+    await step(
+      'Assert the small style on both the Select & Field',
+      async () => {
+        expect(canvas.getByLabelText('Small label')).toHaveClass('text-sm');
+        expect(canvas.getByText('Small label')).toHaveClass('text-xs');
+      },
+    );
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    label: 'Medium label',
+    sizer: Select.sizer.medium,
+    children: <option>Medium Select</option>,
+  },
+  play: async ({ canvas, step }) => {
+    await step(
+      'Assert the medium style on both the Select & Field',
+      async () => {
+        expect(canvas.getByLabelText('Medium label')).toHaveClass('text-base');
+        expect(canvas.getByText('Medium label')).toHaveClass('text-sm');
+      },
+    );
+  },
+};
+
+export const Large: Story = {
+  args: {
+    label: 'Large label',
+    sizer: Select.sizer.large,
+    children: <option>Large Select</option>,
+  },
+  play: async ({ canvas, step }) => {
+    await step(
+      'Assert the small style on both the Select & Field',
+      async () => {
+        expect(canvas.getByLabelText('Large label')).toHaveClass('text-lg');
+        expect(canvas.getByText('Large label')).toHaveClass('text-lg');
+      },
+    );
   },
 };
 
@@ -70,30 +183,6 @@ export const AllControlStates: Story = {
       <Select {...Disabled.args} />
     </div>
   ),
-};
-
-export const Small: Story = {
-  args: {
-    sizer: Select.sizer.small,
-    'aria-label': 'Example small Select',
-    children: <option>Small Select (default)</option>,
-  },
-};
-
-export const Medium: Story = {
-  args: {
-    sizer: Select.sizer.medium,
-    'aria-label': 'Example medium Select',
-    children: <option>Medium Select</option>,
-  },
-};
-
-export const Large: Story = {
-  args: {
-    sizer: Select.sizer.large,
-    'aria-label': 'Example large Select',
-    children: <option>Large Select</option>,
-  },
 };
 
 export const AllSizes: Story = {
