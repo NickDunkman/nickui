7e89437a254d7c53ebed1df823f1d481941e16ec
Nick Dunkman <nickdunkman@gmail.com>
2025-07-13 13:17:11 -0700

    update docs

---
+++ docs/assets/Radio--hxZWM96.js
@@ -0,0 +1,9 @@
+import{j as e,M as p}from"./blocks-NPNNSvdN.js";import{useMDXComponents as m}from"./index-CR7ffIg2.js";import{D as o}from"./index-Bv6FgS2I.js";import{R as s}from"./Radio-EODjI3b0.js";import{S as i}from"./Radio.stories-CETlivay.js";import"./iframe-DQ1lPaZy.js";import"./index-B73xBLE3.js";import"./clsw-CBfrqCZ4.js";import"./Button-CbPv7lzY.js";import"./index-C4eiBceS.js";import"./Checkbox-B06LMJVy.js";import"./CheckedField-C91g2PzS.js";import"./randomId-Dfhrkieu.js";function a(t){const r={p:"p",...m(),...t.components};return o||n("Docs",!1),o.ComponentHeading||n("Docs.ComponentHeading",!0),o.ControlStates||n("Docs.ControlStates",!0),o.FauxCanvas||n("Docs.FauxCanvas",!0),o.FormLibraryCompatibility||n("Docs.FormLibraryCompatibility",!0),o.Props||n("Docs.Props",!0),o.Sizing||n("Docs.Sizing",!0),o.TableOfContents||n("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(p,{of:i}),`
+`,e.jsx(o.ComponentHeading,{of:"Radio"}),`
+`,e.jsx(r.p,{children:"A form control that allow users to choose one option from a set."}),`
+`,e.jsx(o.FauxCanvas,{children:e.jsx(s,{label:"I hope you don’t regret checking me",sizer:s.sizer.medium})}),`
+`,e.jsx(o.TableOfContents,{}),`
+`,e.jsx(o.Props,{of:i,wrapper:"CheckedField",nativeBase:"input"}),`
+`,e.jsx(o.ControlStates,{of:i,checkable:!0}),`
+`,e.jsx(o.Sizing,{of:i,wrapper:"CheckedField"}),`
+`,e.jsx(o.FormLibraryCompatibility,{of:i,fieldName:"inTheHoleFormik",radioWithValue:"holed-out",componentProps:{label:"It’s in the hole"}})]})}function F(t={}){const{wrapper:r}={...m(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(a,{...t})}):a(t)}function n(t,r){throw new Error("Expected "+(r?"component":"object")+" `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{F as default};
--- docs/assets/Radio-C8iNl4ay.js
+++
@@ -1,9 +0,0 @@
-import{j as e,M as p}from"./blocks-CRgQkwTs.js";import{useMDXComponents as m}from"./index-BOS4qjoI.js";import{D as o}from"./index-BZFlQj2E.js";import{R as s}from"./Radio-C5T00fGX.js";import{S as i}from"./Radio.stories-C2GAcBpm.js";import"./iframe-DzZ2548y.js";import"./index-B9m-O1jy.js";import"./clsw-CBfrqCZ4.js";import"./Button-LGCp3pe5.js";import"./index-C4eiBceS.js";import"./Checkbox-DkyyvXiP.js";import"./CheckedField-B62_0hA1.js";import"./randomId-Dfhrkieu.js";function a(t){const r={p:"p",...m(),...t.components};return o||n("Docs",!1),o.ComponentHeading||n("Docs.ComponentHeading",!0),o.ControlStates||n("Docs.ControlStates",!0),o.FauxCanvas||n("Docs.FauxCanvas",!0),o.FormLibraryCompatibility||n("Docs.FormLibraryCompatibility",!0),o.Props||n("Docs.Props",!0),o.Sizing||n("Docs.Sizing",!0),o.TableOfContents||n("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(p,{of:i}),`
-`,e.jsx(o.ComponentHeading,{of:"Radio"}),`
-`,e.jsx(r.p,{children:"A form control that allow users to choose one option from a set."}),`
-`,e.jsx(o.FauxCanvas,{children:e.jsx(s,{label:"I hope you don’t regret checking me",sizer:s.sizer.medium})}),`
-`,e.jsx(o.TableOfContents,{}),`
-`,e.jsx(o.Props,{of:i,wrapper:"CheckedField",nativeBase:"input"}),`
-`,e.jsx(o.ControlStates,{of:i,checkable:!0}),`
-`,e.jsx(o.Sizing,{of:i,wrapper:"CheckedField"}),`
-`,e.jsx(o.FormLibraryCompatibility,{of:i,fieldName:"inTheHoleFormik",radioWithValue:"holed-out",componentProps:{label:"It’s in the hole"}})]})}function F(t={}){const{wrapper:r}={...m(),...t.components};return r?e.jsx(r,{...t,children:e.jsx(a,{...t})}):a(t)}function n(t,r){throw new Error("Expected "+(r?"component":"object")+" `"+t+"` to be defined: you likely forgot to import, pass, or provide it.")}export{F as default};
