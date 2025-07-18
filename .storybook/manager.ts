0635128264b224c3798380b6edb634f03cc634e1
Nick Dunkman <nickdunkman@gmail.com>
2025-07-17 17:28:20 -0700

    customize favicon and site title

---
+++ .storybook/manager.ts
@@ -0,0 +1,30 @@
+import { addons } from 'storybook/manager-api';
+
+const SITE_NAME = 'NickUI';
+
+// Custom addon that adds nicer formatting to the site title & prefixes with
+// "NicKUI".
+addons.register('titleNickUI', (api) => {
+  const setTitle = () => {
+    let storyData;
+    try {
+      storyData = api.getCurrentStoryData();
+    } catch (e) {}
+
+    document.title = !storyData
+      ? SITE_NAME
+      : storyData.type === 'docs'
+        ? `${SITE_NAME} ⋅ ${storyData.title.replace(/\//g, ' ⋅ ')}`
+        : `${SITE_NAME} ⋅ ${storyData.title.replace(/\//g, ' ⋅ ')} ⋅ ${storyData.name}`;
+  };
+
+  return new MutationObserver(() => {
+    if (document.title.endsWith('Storybook')) {
+      setTitle();
+    }
+  }).observe(document.querySelector('title') as Node, {
+    childList: true,
+    subtree: true,
+    characterData: true,
+  });
+});
