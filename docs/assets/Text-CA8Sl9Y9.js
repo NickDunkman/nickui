1f35a9467b6698ecb0d6b0da1aefab95d61eed50
Nick Dunkman <nickdunkman@gmail.com>
2025-07-28 11:49:01 -0700

    update docs

---
+++ docs/assets/Text-CA8Sl9Y9.js
@@ -0,0 +1,9 @@
+import{j as t,M as m}from"./blocks-D7odKEet.js";import{useMDXComponents as p}from"./index-C4jVorX1.js";import{D as o}from"./index-bo37cPEH.js";import{S as i,T as s}from"./Text.stories-BADUXGI8.js";import"./iframe-D7dEraYJ.js";import"./index-YCstKRQY.js";import"./client-BGiythfe.js";import"./index-ua445rCL.js";import"./Button-C8l9t0_m.js";import"./index-9ojHIYZ1.js";import"./AllSizersStoryWrapper-ByqcBRQW.js";import"./Field-BkUVDWgm.js";import"./randomId-Dfhrkieu.js";function a(e){const r={p:"p",...p(),...e.components};return o||n("Docs",!1),o.ComponentHeading||n("Docs.ComponentHeading",!0),o.ControlStates||n("Docs.ControlStates",!0),o.FauxCanvas||n("Docs.FauxCanvas",!0),o.FormLibraryCompatibility||n("Docs.FormLibraryCompatibility",!0),o.Props||n("Docs.Props",!0),o.Sizers||n("Docs.Sizers",!0),o.TableOfContents||n("Docs.TableOfContents",!0),t.jsxs(t.Fragment,{children:[t.jsx(m,{of:i}),`
+`,t.jsx(o.ComponentHeading,{of:"Text"}),`
+`,t.jsx(r.p,{children:"A form control that allows users to enter & edit a single line of text."}),`
+`,t.jsx(o.FauxCanvas,{children:t.jsx(s,{placeholder:"Type something!"})}),`
+`,t.jsx(o.TableOfContents,{}),`
+`,t.jsx(o.Props,{of:i,nativeBase:"input",wrapper:"Field"}),`
+`,t.jsx(o.ControlStates,{of:i,valuable:!0}),`
+`,t.jsx(o.Sizers,{of:i,wrapper:"Field"}),`
+`,t.jsx(o.FormLibraryCompatibility,{of:i,fieldName:"player",initialValue:"Nick",component:s,componentProps:{label:"Player name",hint:"Try editting me!"}})]})}function F(e={}){const{wrapper:r}={...p(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(a,{...e})}):a(e)}function n(e,r){throw new Error("Expected "+(r?"component":"object")+" `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{F as default};
---
+++ docs/assets/index-YCstKRQY.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-D7dEraYJ.js";import{r as o}from"./client-BGiythfe.js";var t=o();const m=r(t);export{m as R,t as r};
