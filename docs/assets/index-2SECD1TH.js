d802ec55f7afd9b5e36b31a791f7a6d2c0aa70c2
Nick Dunkman <nickdunkman@gmail.com>
2025-07-28 13:02:48 -0700

    update docs

--- docs/assets/index-2SECD1TH.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-D3szbWiH.js";import{r as o}from"./client-BywA0A9E.js";var t=o();const m=r(t);export{m as R,t as r};
---
+++ docs/assets/index-Bw8B5bNi.js
@@ -0,0 +1,1 @@
+import{g as r}from"./iframe-DJUO-rvG.js";import{r as o}from"./client-BvYMHd_H.js";var t=o();const m=r(t);export{m as R,t as r};
