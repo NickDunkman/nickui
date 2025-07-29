b46451685cd42ad9ddd49a1592e6254496cb5009
Nick Dunkman <nickdunkman@gmail.com>
2025-07-28 17:29:00 -0700

    update docs

--- docs/assets/Text-dPdx6Vaw.js
+++
@@ -1,9 +0,0 @@
-import{j as t,M as m}from"./blocks-T_PcORyW.js";import{useMDXComponents as p}from"./index-3EBmbjCJ.js";import{D as o}from"./index-DXZmtTEL.js";import{S as i,T as s}from"./Text.stories-Dz3csiC0.js";import"./iframe-ewlT1mRj.js";import"./index-tB6CmcNF.js";import"./client-B0KyhFsI.js";import"./index-uDilQlfd.js";import"./Button-CZe0l6yE.js";import"./index-DVh8ZukV.js";import"./AllSizersStoryWrapper-Do39ytD5.js";import"./Field-BijVQz4q.js";import"./randomId-Dfhrkieu.js";function a(e){const r={p:"p",...p(),...e.components};return o||n("Docs",!1),o.ComponentHeading||n("Docs.ComponentHeading",!0),o.ControlStates||n("Docs.ControlStates",!0),o.FauxCanvas||n("Docs.FauxCanvas",!0),o.FormLibraryCompatibility||n("Docs.FormLibraryCompatibility",!0),o.Props||n("Docs.Props",!0),o.Sizers||n("Docs.Sizers",!0),o.TableOfContents||n("Docs.TableOfContents",!0),t.jsxs(t.Fragment,{children:[t.jsx(m,{of:i}),`
-`,t.jsx(o.ComponentHeading,{of:"Text"}),`
-`,t.jsx(r.p,{children:"A form control that allows users to enter & edit a single line of text."}),`
-`,t.jsx(o.FauxCanvas,{children:t.jsx(s,{placeholder:"Type something!"})}),`
-`,t.jsx(o.TableOfContents,{}),`
-`,t.jsx(o.Props,{of:i,nativeBase:"input",wrapper:"Field"}),`
-`,t.jsx(o.ControlStates,{of:i,valuable:!0}),`
-`,t.jsx(o.Sizers,{of:i,wrapper:"Field"}),`
-`,t.jsx(o.FormLibraryCompatibility,{of:i,fieldName:"player",initialValue:"Nick",component:s,componentProps:{label:"Player name",hint:"Try editting me!"}})]})}function F(e={}){const{wrapper:r}={...p(),...e.components};return r?t.jsx(r,{...e,children:t.jsx(a,{...e})}):a(e)}function n(e,r){throw new Error("Expected "+(r?"component":"object")+" `"+e+"` to be defined: you likely forgot to import, pass, or provide it.")}export{F as default};
