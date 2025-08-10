488344d9f40d13649b05fb1dc7a51b5c80ea9e14
Nick Dunkman <nickdunkman@gmail.com>
2025-08-10 14:39:55 -0700

    add TanstackForm tests

--- src/dev/tests/formLibraryTests.tsx
+++ src/dev/tests/formLibraryTests.tsx
@@ -1,11 +1,13 @@
+import { useForm as useTanstackForm } from '@tanstack/react-form';
 import { useFormik } from 'formik';
 import * as React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm as useReactHookForm } from 'react-hook-form';
 
 import { FieldableProps } from '@/types';
 
 export const formLibraryTests = [
   { Test: ReactHookFormTest, library: 'React Hook Form' },
+  { Test: TanstackFormTest, library: 'Tanstack Form' },
   { Test: FormikTest, library: 'Formik' },
 ] as const;
 
@@ -25,7 +27,7 @@ export function ReactHookFormTest<T extends React.JSX.IntrinsicAttributes>({
   const {
     register,
     formState: { errors, touchedFields },
-  } = useForm({
+  } = useReactHookForm({
     mode: 'all',
     defaultValues: { [fieldName]: initialValue },
   });
@@ -39,6 +41,52 @@ export function ReactHookFormTest<T extends React.JSX.IntrinsicAttributes>({
       data-touched={!!touchedFields[fieldName]}
       {...componentProps}
     />
+  );
+}
+
+export function TanstackFormTest<T extends React.JSX.IntrinsicAttributes>({
+  Component,
+  fieldName,
+  initialValue,
+  erroneousValue,
+  componentProps,
+}: {
+  Component: React.ComponentType<T>;
+  fieldName: string;
+  initialValue: string;
+  erroneousValue: string;
+  componentProps: T;
+}) {
+  const form = useTanstackForm({
+    defaultValues: {
+      [fieldName]: initialValue,
+    },
+  });
+
+  return (
+    <form.Field
+      name={fieldName}
+      validators={{
+        onChange: ({ value }) =>
+          value === erroneousValue ? 'That’s erroneous' : undefined,
+      }}
+    >
+      {(field) => (
+        <Component
+          name={field.name}
+          value={field.state.value}
+          onBlur={field.handleBlur}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            field.handleChange(event.target.value);
+          }}
+          error={
+            !field.state.meta.isValid && field.state.meta.errors.join(', ')
+          }
+          data-touched={!!field.state.meta.isTouched}
+          {...componentProps}
+        />
+      )}
+    </form.Field>
   );
 }
 
