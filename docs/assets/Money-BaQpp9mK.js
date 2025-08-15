d148800a0879c5c19ababf4f226c71c42104f8c5
Nick Dunkman <nickdunkman@gmail.com>
2025-08-15 13:54:04 -0700

    update docs

---
+++ docs/assets/Money-BaQpp9mK.js
@@ -0,0 +1,82 @@
+import{j as e,M as p,C as a,S as i}from"./blocks-BybPGZnz.js";import{useMDXComponents as u}from"./index-CxeJwy2m.js";import{D as t}from"./index-BPy4f3jE.js";import{M as l}from"./Money-BFscZdFF.js";import{s as r,c,a as f}from"./Money.stories-DKNfG-g_.js";import"./preload-helper-D9Z9MdNV.js";import"./iframe-ClCKDRnM.js";import"./index-45EnxtDN.js";import"./client-ae6XlYz6.js";import"./Button-BfdKq3oR.js";import"./index-Bcc7r1sS.js";import"./index-C9N6JpZ7.js";import"./Field-CkGsrfN-.js";import"./styles-DKXCIM0g.js";import"./fieldA11y-D5KJLKJw.js";import"./randomId-Dfhrkieu.js";import"./useElementBounds-D7v76WMO.js";import"./fireInputChange-BU1kNzof.js";import"./AllSizersStoryWrapper-CyZMSWq_.js";import"./getStoryArgTypes-msA8qnCj.js";function d(o){const n={a:"a",code:"code",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...u(),...o.components};return t||s("Docs",!1),t.ComponentHeading||s("Docs.ComponentHeading",!0),t.ControlStates||s("Docs.ControlStates",!0),t.FauxCanvas||s("Docs.FauxCanvas",!0),t.FormLibraryDemos||s("Docs.FormLibraryDemos",!0),t.Props||s("Docs.Props",!0),t.Sizers||s("Docs.Sizers",!0),t.TableOfContents||s("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(p,{of:r}),`
+`,e.jsx(t.ComponentHeading,{of:"Money"}),`
+`,e.jsx(n.p,{children:"A form control that allows users to enter a monetary amount."}),`
+`,e.jsx(n.p,{children:`The displayed value is automatically formatted according to the specified
+configuration, though will “deformat” as you edit the field, since it can feel
+disorienting when the changes don’t match your keystrokes.`}),`
+`,e.jsxs(n.p,{children:[`If you’re interested, check out the
+`,e.jsx(n.a,{href:"/docs/form-controls-money--how-it-works",children:"How it works"})," write-up!"]}),`
+`,e.jsx(t.FauxCanvas,{children:e.jsx(l,{defaultValue:"1234567.89"})}),`
+`,e.jsx(t.TableOfContents,{}),`
+`,e.jsx(t.Props,{of:r,nativeBase:"input",wrapper:"Field"}),`
+`,e.jsx(t.ControlStates,{of:r,valuable:!0,note:`
+  Note: when controlled, <ComponentName of="Money" /> behaves slightly
+  differently from other form controls. Since the input’s value needs to be
+  disconnected from the controlled \`value\` to enable currency formatting,
+  the input’s value can change even if you aren’t passing in the new
+  controlled value. However, if you do pass in a numerically different
+  controlled value, everything will cascade accordingly.
+`}),`
+`,e.jsx(n.h2,{id:"currency-formatting--constraints",children:"Currency formatting & constraints"}),`
+`,e.jsxs(n.p,{children:[`To customize the currency format, you can use a combination of the
+`,e.jsx(n.code,{children:"currencySymbol"}),", ",e.jsx(n.code,{children:"decimalPoint"}),", and ",e.jsx(n.code,{children:"thousandsSeparator"}),` props. They all use
+USD defaults.`]}),`
+`,e.jsxs(n.p,{children:["The ",e.jsx(n.code,{children:"decimalPlaces"})," prop defaults to ",e.jsx(n.code,{children:"2"})," and configures:"]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:"the maximum number of decimal places the user can enter"}),`
+`,e.jsx(n.li,{children:'the number of "placeholder" decimal places that are suggested as you type'}),`
+`,e.jsxs(n.li,{children:["the number of decimal places always included on ",e.jsx(n.code,{children:"onChange(event.target.value)"})]}),`
+`]}),`
+`,e.jsxs(n.p,{children:[e.jsx(n.code,{children:"decimalPlaces"}),` also takes a min/max object, if you want to allow the user to
+add even more decimal places than is suggested by the placeholder. You can see
+that in action here, where the Bitcoin field suggests 8 decimal places, but the
+user can add up to 10.`]}),`
+`,e.jsx(a,{of:c,source:{language:"tsx",code:c.parameters.source}}),`
+`,e.jsxs(n.p,{children:[`By default, negative values are not allowed, since typically, a monetary field
+is either positive (like in a deposit form), or negative represented with a
+positive value (like in a withdrawal form). However, you can allow negatives
+using the `,e.jsx(n.code,{children:"allowNegatives"})," prop."]}),`
+`,e.jsx(n.p,{children:`Keep in mind that if you allow negatives, the user won’t be able to use the
+simpler numeric keyboard on mobile devices, since it doesn’t include a key for
+the negative sign.`}),`
+`,e.jsx(a,{of:f}),`
+`,e.jsx(n.h2,{id:"working-with-the-value",children:"Working with the value"}),`
+`,e.jsxs(n.p,{children:["You can pass any of these forms in through either the ",e.jsx(n.code,{children:"value"})," or ",e.jsx(n.code,{children:"defaultValue"}),`
+props:`]}),`
+`,e.jsxs(n.ul,{children:[`
+`,e.jsx(n.li,{children:e.jsx(n.code,{children:"undefined"})}),`
+`,e.jsxs(n.li,{children:["a ",e.jsx(n.code,{children:"number"})]}),`
+`,e.jsxs(n.li,{children:["a numeric ",e.jsx(n.code,{children:"string"})," such as ",e.jsx(n.code,{children:'"1234.56"'})," or ",e.jsx(n.code,{children:'""'}),`. It should be parseable as a
+`,e.jsx(n.code,{children:"Number()"}),". For example, while ",e.jsx(n.code,{children:'"17,50"'}),` is how a number might be formatted in
+many countries, it’s not a number as far as javascript is concerned (`,e.jsx(n.code,{children:'"17.50"'}),`
+is).`]}),`
+`]}),`
+`,e.jsxs(n.p,{children:["When the user updates edits the field, use the ",e.jsx(n.code,{children:"onChange"}),` handler to receive the
+updated value. `,e.jsx(n.code,{children:"event.target.value"})," is ",e.jsx(n.strong,{children:"always"}),` a string in javascript, so the
+updated value will be in the form of a string.`]}),`
+`,e.jsx(n.p,{children:"You might choose to just use strings through, like this:"}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+  const [value, setValue] = React.useState('1234.56');
+
+  <Money
+    label="My money field"
+    value={value}
+    onChange={(event) => setValue(event.target.value)}
+  />
+
+`}),`
+`,e.jsx(n.p,{children:"Or, you could convert updated values back to numbers, like this:"}),`
+`,e.jsx(i,{dark:!0,language:"tsx",code:`
+  const [value, setValue] = React.useState<number | undefined>(1234.56);
+
+  <Money
+    label="My money field"
+    value={value}
+    onChange={(event) => setValue(
+      event.target.value === '' ? undefined : Number(event.target.value)
+    )}
+  />
+
+`}),`
+`,e.jsx(t.Sizers,{of:r,wrapper:"Field"}),`
+`,e.jsx(t.FormLibraryDemos,{of:"Money",fieldName:"greensFees",initialValue:"137.00",children:({props:h,error:m})=>e.jsx(l,{...h,label:"Greens fees",hint:"Try editting me!",error:m})})]})}function O(o={}){const{wrapper:n}={...u(),...o.components};return n?e.jsx(n,{...o,children:e.jsx(d,{...o})}):d(o)}function s(o,n){throw new Error("Expected "+(n?"component":"object")+" `"+o+"` to be defined: you likely forgot to import, pass, or provide it.")}export{O as default};
---
+++ docs/assets/Switches-CzdTbwra.js
@@ -0,0 +1,67 @@
+import{j as e,M as p,C as n,S as r}from"./blocks-BybPGZnz.js";import{useMDXComponents as d}from"./index-CxeJwy2m.js";import{D as o}from"./index-BPy4f3jE.js";import{s as i,S as l,a as m,c as x}from"./Switches.stories-BKK3_An3.js";import"./preload-helper-D9Z9MdNV.js";import"./iframe-ClCKDRnM.js";import"./index-45EnxtDN.js";import"./client-ae6XlYz6.js";import"./Button-BfdKq3oR.js";import"./index-Bcc7r1sS.js";import"./index-C9N6JpZ7.js";import"./Switch-DT9rUP7K.js";import"./CheckableFormControl-CN_kNOwB.js";import"./fieldA11y-D5KJLKJw.js";import"./randomId-Dfhrkieu.js";import"./CheckedField-BBWefoHK.js";import"./useElementBounds-D7v76WMO.js";import"./AllSizersStoryWrapper-CyZMSWq_.js";import"./getStoryArgTypes-msA8qnCj.js";import"./CheckableFormControls-B5o7idW_.js";import"./Fieldset-BB2YWFx3.js";function c(s){const t={code:"code",h2:"h2",p:"p",span:"span",strong:"strong",...d(),...s.components};return o||a("Docs",!1),o.ComponentHeading||a("Docs.ComponentHeading",!0),o.ControlStates||a("Docs.ControlStates",!0),o.FauxCanvas||a("Docs.FauxCanvas",!0),o.FormLibraryDemos||a("Docs.FormLibraryDemos",!0),o.Props||a("Docs.Props",!0),o.Sizers||a("Docs.Sizers",!0),o.TableOfContents||a("Docs.TableOfContents",!0),e.jsxs(e.Fragment,{children:[e.jsx(p,{of:i}),`
+`,e.jsx(o.ComponentHeading,{of:"Switches"}),`
+`,e.jsx("div",{children:e.jsxs(t.p,{children:["Renders multiple ",e.jsx(o,{of:"Switch"})," that perform as one field in a"," ",`
+`,e.jsx(o,{of:"Fieldset"}),"."]})}),`
+`,e.jsx(o.FauxCanvas,{children:e.jsx(l,{label:"Notifications",defaultValue:"major,minor",options:[{value:"major",label:e.jsxs(e.Fragment,{children:[e.jsx(t.strong,{children:"Major"}),e.jsx(t.span,{className:"text-gray-400","aria-hidden":!0,children:".x.x"})," ","releases"]})},{value:"minor",label:e.jsxs(e.Fragment,{children:[e.jsx(t.span,{className:"text-gray-400","aria-hidden":!0,children:"x."}),e.jsx(t.strong,{children:"Minor"}),e.jsx(t.span,{className:"text-gray-400","aria-hidden":!0,children:".x"})," ","releases"]})},{value:"patch",label:e.jsxs(e.Fragment,{children:[e.jsx(t.span,{className:"text-gray-400","aria-hidden":!0,children:"x.x."}),e.jsx(t.strong,{children:"Patch"})," releases"]})}]})}),`
+`,e.jsx(o.TableOfContents,{}),`
+`,e.jsx(o.Props,{of:i,nativeBase:"input",wrapper:"Fieldset"}),`
+`,e.jsx(o.ControlStates,{of:i,valuable:!0,sourceState:"hidden",note:"Note: while you can disable individual switches as shown in sections below, using `disabled={true}` will disable the entire field by forcing all child switches to be disabled."}),`
+`,e.jsx(t.h2,{id:"adding-switches",children:"Adding switches"}),`
+`,e.jsxs(t.p,{children:["For the standard look & feel, use the ",e.jsx(t.code,{children:"options"}),` prop to configure the individual
+switches. `,e.jsx(o,{of:"Switches"})," will manage the interior layout."]}),`
+`,e.jsx(n,{of:m,sourceState:"shown"}),`
+`,e.jsxs(t.p,{children:[`To customize the switch controls, such as to use a different component for the
+individual switches, call the props differently, or add other elements, you can
+pass a `,e.jsx(t.code,{children:"render"})," function instead of using ",e.jsx(t.code,{children:"options"}),"."]}),`
+`,e.jsx("strong",{children:e.jsx(t.p,{children:`As shown, it’s important to use the callback provided to pass props down to
+the individual switches, so that group is functional.`})}),`
+`,e.jsx(n,{of:x,sourceState:"none"}),`
+`,e.jsx(r,{dark:!0,language:"tsx",code:`
+  <Switches
+    label="Please select some customizations"
+    defaultValue="colorful,embiggened"
+    render={(checkbox) => (
+      <div className="flex flex-row items-baseline gap-x-6">
+        <Switch
+          {...checkbox({ value: 'colorful' })}
+          label={<span className="font-bold text-blue-700">Colorful</span>}
+          className="rounded-sm bg-rose-200 p-2"
+        />
+        <Switch
+          {...checkbox({ value: 'embiggened' })}
+          sizer="lg"
+          label="Embiggened"
+        />
+        <div>
+          <input type="checkbox" {...checkbox({ value: 'native' })} /> Native
+        </div>
+      </div>
+    )}
+  />
+`}),`
+`,e.jsx(t.h2,{id:"working-with-the-value",children:"Working with the value"}),`
+`,e.jsx(t.p,{children:`The purpose of this component is to make it as easy as possible to have a
+consistent look & feel for fields with multiple switches. In order to simplify
+the boilerplate needed for libraries like Formik and React Hook Form, the
+component’s value is a single delimited-string, & toggling each individual
+switch adds or removes a particular substring.`}),`
+`,e.jsx(t.p,{children:`If you want to work with an array of strings instead of a combined string, you
+can convert to/from an array somewhere higher up the tree, as shown in this
+example:`}),`
+`,e.jsx(r,{dark:!0,language:"tsx",code:`
+  const [value, setValue] = React.useState<string[]>(['a', 'c']);
+  //...
+  <Switches
+    options={[
+      { value: 'a', label: 'A' },
+      { value: 'b', label: 'B' },
+      { value: 'c', label: 'D' },
+    ]}
+    value={value.join(',')}
+    onChange={(event) => {
+      setValue(event.target.value.split(','));
+    }}
+  />
+`}),`
+`,e.jsx(o.Sizers,{of:i,wrapper:"Fieldset"}),`
+`,e.jsx(o.FormLibraryDemos,{of:"Switches",fieldName:"preShotRoutine",children:({props:h,error:u})=>e.jsx(l,{...h,label:"Pre-shot routine",hint:"Try editting me!",error:u,options:[{value:"lie",label:"1. Evaluate the lie"},{value:"imagine",label:"2. Imagine the shot"},{value:"target",label:"3. Pick a target"},{value:"club",label:"4. Choose club"},{value:"position",label:"5. Position stance relative to the ball & shot type"},{value:"align",label:"6. Align feet, hips, shoulders to the target"},{value:"flare",label:"7. Flare front foot without affecting other body alignment"},{value:"grip",label:"8. Grip the club according to the shot type"},{value:"waggle",label:"9. One club-path waggle"},{value:"fixate",label:"10. Fixate mind on target"},{value:"go",label:"11. Go"}]})})]})}function H(s={}){const{wrapper:t}={...d(),...s.components};return t?e.jsx(t,{...s,children:e.jsx(c,{...s})}):c(s)}function a(s,t){throw new Error("Expected "+(t?"component":"object")+" `"+s+"` to be defined: you likely forgot to import, pass, or provide it.")}export{H as default};
