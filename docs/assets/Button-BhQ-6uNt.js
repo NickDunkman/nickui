2d3e39dc41a279f3f8a55765bb23723c4430f123
Nick Dunkman <nickdunkman@gmail.com>
2025-08-16 13:23:01 -0700

    update docs

---
+++ docs/assets/Button-BhQ-6uNt.js
@@ -0,0 +1,9 @@
+import{j as t,M as m}from"./blocks-BCVByPfQ.js";import{useMDXComponents as a}from"./index-cyOj-pa3.js";import{D as o}from"./index-cOmBS43N.js";import{B as f}from"./Button-D7LzSOLp.js";import{s as r}from"./Button.stories-8_m2sIE3.js";import"./preload-helper-D9Z9MdNV.js";import"./iframe-iALQ-ti1.js";import"./index-DTZDYbKY.js";import"./client-BFIn-svK.js";import"./index-D8gdkzdT.js";import"./index-Dad_cwmR.js";import"./AllSizersStoryWrapper-D4nj99Nk.js";import"./getStoryArgTypes-q4o9iYFY.js";function i(n){const s={p:"p",...a(),...n.components};return o||e("Docs",!1),o.ComponentHeading||e("Docs.ComponentHeading",!0),o.ControlStates||e("Docs.ControlStates",!0),o.FauxCanvas||e("Docs.FauxCanvas",!0),o.Flavoring||e("Docs.Flavoring",!0),o.Props||e("Docs.Props",!0),o.Sizers||e("Docs.Sizers",!0),o.TableOfContents||e("Docs.TableOfContents",!0),t.jsxs(t.Fragment,{children:[t.jsx(m,{of:r}),`
+`,t.jsx(o.ComponentHeading,{of:"Button"}),`
+`,t.jsx(s.p,{children:"Allows the user to trigger an action, such as submitting a form."}),`
+`,t.jsx(o.FauxCanvas,{children:t.jsx(f,{children:"Click me?"})}),`
+`,t.jsx(o.TableOfContents,{}),`
+`,t.jsx(o.Props,{of:r,nativeBase:"button"}),`
+`,t.jsx(o.ControlStates,{of:r}),`
+`,t.jsx(o.Flavoring,{of:r}),`
+`,t.jsx(o.Sizers,{of:r})]})}function F(n={}){const{wrapper:s}={...a(),...n.components};return s?t.jsx(s,{...n,children:t.jsx(i,{...n})}):i(n)}function e(n,s){throw new Error("Expected "+(s?"component":"object")+" `"+n+"` to be defined: you likely forgot to import, pass, or provide it.")}export{F as default};
--- docs/assets/index-DPkyb4R0.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./iframe-DHsgvQHS.js";import{r as o}from"./client-W-Z4jn2C.js";var t=o();const m=r(t);export{m as R,t as r};
